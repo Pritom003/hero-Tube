78a72add082d9385a7b016a6af8df69271a9fd4a
Your NamePritom003 <njahanpritom65@gmail.com>
2023-09-01 19:08:55 +0600

    added nav blog

--- script/script.js
+++ script/script.js
@@ -9,9 +9,11 @@ const catagoryarray=(datas)=>{
   const arrayOfData=datas.data
   console.log(arrayOfData)
   const catgBtn=document.getElementById('catagorybutton')
+  
   const ul=document.createElement('ul')
   ul.classList=`flex gap-4 px-8  md:px- lg:px-96  `
  let liHTML='';
+
   arrayOfData.forEach(element => {
   console.log(element)
   
@@ -89,3 +91,48 @@ const showimage=(click)=>{
   image.classList.remove('hidden')
   console.log(click)
 }
+// navbaar
+
+// --------------QNA___________
+
+  document.getElementById('blogbutton').addEventListener( 'click',function (){
+    const div=document.createElement('div')
+  const divOne=document.createElement('div')
+  divOne.innerHTML=`
+  <div class="px-8">
+      <h1 class="text-6xl"> Questions</h1>
+      <ul class="grid gap-8 text-xs text-center">
+        <li>
+        <h1 class="text-xl">QUESTION-01:</h1>
+        Discuss the scope of var, let, and const</li>
+        <li>
+        <h1 class="text-xl">QUESTION-01:</h1>
+        Tell us the use cases of null and undefined</li>
+        <li>
+        <h1 class="text-xl">QUESTION-01:</h1>
+        What do you mean by REST API?</li>
+        
+      </ul>
+    </div>
+      <div class="px-8">
+      <h1 class="text-6xl">Answers</h1>
+      <ul class="grid gap-8 text-xs text-center">
+        <li>
+        <h1 class="text-xl">Answer-01:</h1>
+          var declarations are globally scoped or function scoped while let and const are block scoped. var variables can be updated and re-declared within its scope; let variables can be updated but not re-declared; const variables 
+          can neither be updated nor re-declared.
+        </li>
+        <li> <h1 class="text-xl">Answer-02:</h1>
+          Undefined means the variable has been declared, but its value has not been assigned. Null means an empty value or a blank value. The typeof() operator returns undefined for an undefined variable. The typeof() operator returns the type as an object for a variable whose
+           value is assigned as null.
+        </li>
+        <li> <h1 class="text-xl">Answer-03:</h1>
+        An API, or application programming interface, is a set of rules that define how applications or devices can connect to and communicate with each other. A REST API is an API that conforms to the design principles of the REST, 
+          or representational state transfer architectural style.</li>
+      </ul>
+    </div>
+  `
+  const content=document.body.appendChild(divOne);
+  content=''
+  
+  })
