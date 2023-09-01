ba89736df24a23a23deb65f34a2d624988ab79bf
Your NamePritom003 <njahanpritom65@gmail.com>
2023-09-01 17:38:41 +0600

    added varification

--- script/script.js
+++ script/script.js
@@ -10,14 +10,20 @@ const catagoryarray=(datas)=>{
   console.log(arrayOfData)
   const catgBtn=document.getElementById('catagorybutton')
   const ul=document.createElement('ul')
-  ul.classList=`grid grid-cols-4 gap 2`
+  ul.classList=`flex gap-4 px-8  md:px- lg:px-96  `
  let liHTML='';
   arrayOfData.forEach(element => {
   console.log(element)
   
-   liHTML +=`<li><a onclick="categoryId('${element.category_id}')" href='#' class="btn btn-outline">${element.category}</a></li>`
-
+  //  liHTML +=`<li><a onclick="categoryId('${element.category_id}')" href='#' class="btn btn-outline">
+  //  ${element.category}</a></li>`
  
+
+    liHTML += `<li><a onclick="${element.category === 'Drawing' ? `oNCLICK5('${element.category_id}')`
+     : `categoryId('${element.category_id}')`}" href='#' class="btn btn-outline">
+    ${element.category}
+</a></li>`;
+
 
   })
   ul.innerHTML=liHTML
@@ -26,15 +32,61 @@ const catagoryarray=(datas)=>{
 }
 // creating oncllick data____---
 const categoryId=async(ctgoryId)=>{
+  console.log(ctgoryId)
   
   const resctgId =await fetch(`https://openapi.programming-hero.com/api/videos/category/${ctgoryId}`)
   const datactgId=await resctgId.json()
 console.log(datactgId.data)
 const datactgIdarray=datactgId.data;
-datactgIdarray.forEach=(info)=>{
+const mainDIV=document.getElementById('maincontent')
+mainDIV.innerHTML='';
+datactgIdarray.forEach(info=>{
   console.log(info)
-  const mainDIV=document.getElementById('maincontent')
+  const createdDiv=document.createElement('div')
+
+  
+  
+  createdDiv.innerHTML=`
+  <div class="card bg-base-100 shadow-xl">
+          <figure><img class=" w-full lg:w-96 lg:h-64" src="${info.thumbnail}" alt=thumbnail /></figure>
+          <div class="card-body">
+            <div class="flex gap-3">
+              <img class="rounded-full w-10 h-10   "  src="${info.authors[0].profile_picture}" alt="dp">
+              <div>
+                <h2>${info.title.slice(0,12)}
+                </h2>
+              <p>
+                ${info.authors[0].profile_name}
+                <span>
+                
+                ${info.authors[0].verified ? `<img  class="inline " src="style/tik (1).png" alt="Verified">` : ''}
+               </span>
+       
+              </p>
+              <span class='text-sm'> 91K views</span>
+            </div>
+            </div>
+            
+          </div>
+        </div>`;
+        mainDIV.appendChild(createdDiv)
+})
+        
 }
   
-}
+
+
 HandleCatagory()
+categoryId('01')
+// ONCLICK 
+const oNCLICK5=(click)=>{
+  const div=document.getElementById("sorrydib")
+  div.classList.remove('hidden')
+  console.log(click)
+  console.log('HI')
+}
+const showimage=(click)=>{
+  const image=document.getElementById('tikimage')
+  image.classList.remove('hidden')
+  console.log(click)
+}
